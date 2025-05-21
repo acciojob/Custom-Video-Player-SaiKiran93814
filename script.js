db82842799a093a4363101dffda603a158a4af22
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-05-21 22:03:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,60 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+function handleVolumeUpdate() {
+  video.volume = volumeSlider.value;
+}
+
+function handlePlaybackRateUpdate() {
+  video.playbackRate = playbackSlider.value;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+// Event Listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+volumeSlider.addEventListener('input', handleVolumeUpdate);
+playbackSlider.addEventListener('input', handlePlaybackRateUpdate);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+progress.addEventListener('click', scrub);
+let mousedown = false;
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => (mousedown = true));
+progress.addEventListener('mouseup', () => (mousedown = false));
+
+// Error Handling
+video.addEventListener('error', () => {
+  errorMessage.style.display = 'block';
+});
